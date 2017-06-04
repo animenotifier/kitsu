a1c521de196ab331a8a06015a9d97b56cb892c33
Eduard Urbach <e.urbach@gmail.com>
2017-06-04 21:54:58 +0200

    Added FixImageURL

--- Utils.go
+++ Utils.go
@@ -50,3 +50,14 @@ func AllAnime() chan *Anime {
 
 	return channel
 }
+
+// FixImageURL removes the right-most part, e.g. "?1416336000" from image URLs.
+func FixImageURL(url string) string {
+	questionMark := strings.IndexByte(url, '?')
+
+	if questionMark == -1 {
+		return url
+	}
+
+	return url[:questionMark]
+}
