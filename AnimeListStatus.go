81d00f4c983aa1cffb5e7fb6a0abd7088e67998c
Eduard Urbach <e.urbach@gmail.com>
2017-07-12 07:33:17 +0200

    Added list status constants

---
+++ AnimeListStatus.go
@@ -0,0 +1,10 @@
+package kitsu
+
+// AnimeListStatus values for anime list items
+const (
+	AnimeListStatusWatching  = "current"
+	AnimeListStatusCompleted = "completed"
+	AnimeListStatusPlanned   = "planned"
+	AnimeListStatusHold      = "on_hold"
+	AnimeListStatusDropped   = "dropped"
+)
