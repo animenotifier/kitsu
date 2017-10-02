29149cca46057662adea409640ec4b3cec6e4170
Eduard Urbach <e.urbach@gmail.com>
2017-10-02 06:14:08 +0200

    Removed test output

--- kitsu_test.go
+++ kitsu_test.go
@@ -1,16 +1,12 @@
 package kitsu
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/fatih/color"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAnimePage(t *testing.T) {
-	color.Yellow("Fetching first 5 anime titles")
-
 	page, err := GetAnimePage("anime?page[limit]=5&page[offset]=0")
 
 	if err != nil {
@@ -18,32 +14,19 @@ func TestAnimePage(t *testing.T) {
 	}
 
 	for _, anime := range page.Data {
-		fmt.Println("[", anime.ID, "]", anime.Attributes.Titles.EnJp)
-	}
-}
-
-func TestCharacterPage(t *testing.T) {
-	color.Yellow("Fetching first 5 anime titles")
-
-	page, err := GetCharacterPage("characters?page[limit]=5&page[offset]=0")
-
-	if err != nil {
-		panic(err)
-	}
-
-	for _, character := range page.Data {
-		fmt.Println("[", character.ID, "]", character.Attributes.Name)
+		assert.NotEmpty(t, anime.ID)
+		assert.NotEmpty(t, anime.Attributes.Titles.EnJp)
 	}
 }
 
 func TestStreamAnime(t *testing.T) {
-	color.Yellow("Fetching all anime (stops after 40 anime)")
-
 	allAnime := StreamAnime()
 
 	count := 0
 	for anime := range allAnime {
-		fmt.Println("[", anime.ID, "]", anime.Attributes.Titles.EnJp)
+		assert.NotEmpty(t, anime.ID)
+		assert.NotEmpty(t, anime.Attributes.Titles.EnJp)
+
 		count++
 
 		if count >= 40 {
@@ -53,16 +36,15 @@ func TestStreamAnime(t *testing.T) {
 }
 
 func TestStreamAnimeMappings(t *testing.T) {
-	color.Yellow("Fetching all anime with mappings (stops after 40 anime)")
-
 	allAnime := StreamAnimeWithMappings()
 
 	count := 0
 	for anime := range allAnime {
-		fmt.Println("[", anime.ID, "]")
+		assert.NotEmpty(t, anime.ID)
 
 		for _, mapping := range anime.Mappings {
-			fmt.Println(mapping.Attributes.ExternalSite, mapping.Attributes.ExternalID)
+			assert.NotEmpty(t, mapping.Attributes.ExternalSite)
+			assert.NotEmpty(t, mapping.Attributes.ExternalID)
 		}
 
 		count++
@@ -73,14 +55,27 @@ func TestStreamAnimeMappings(t *testing.T) {
 	}
 }
 
-func TestStreamCharacters(t *testing.T) {
-	color.Yellow("Fetching all characters (stops after 40 characters)")
+func TestCharacterPage(t *testing.T) {
+	page, err := GetCharacterPage("characters?page[limit]=5&page[offset]=0")
 
+	if err != nil {
+		panic(err)
+	}
+
+	for _, character := range page.Data {
+		assert.NotEmpty(t, character.ID)
+		assert.NotEmpty(t, character.Attributes.Name)
+	}
+}
+
+func TestStreamCharacters(t *testing.T) {
 	allCharacters := StreamCharacters()
 
 	count := 0
 	for character := range allCharacters {
-		fmt.Println("[", character.ID, "]", character.Attributes.Name)
+		assert.NotEmpty(t, character.ID)
+		assert.NotEmpty(t, character.Attributes.Name)
+
 		count++
 
 		if count >= 40 {
@@ -97,7 +92,6 @@ func TestGetUser(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotEmpty(t, user.ID)
 	assert.NotEmpty(t, user.Attributes.Name)
-
 }
 
 func TestGetUserError(t *testing.T) {
@@ -109,8 +103,6 @@ func TestGetUserError(t *testing.T) {
 }
 
 func TestStreamUserLibrary(t *testing.T) {
-	color.Yellow("Fetching all library entries (stops after 40 entries)")
-
 	userName := "Akyoto"
 	user, _ := GetUser(userName)
 	library := user.StreamLibraryEntries()
@@ -120,10 +112,11 @@ func TestStreamUserLibrary(t *testing.T) {
 		assert.NotNil(t, entry)
 		assert.NotEmpty(t, entry.Attributes.Status)
 
+		assert.NotEmpty(t, entry.ID)
+		assert.NotEmpty(t, entry.Attributes.Status)
+
 		if entry.Anime != nil {
-			fmt.Println("[", entry.ID, "]", entry.Attributes.Status, "|", entry.Anime.Attributes.CanonicalTitle)
-		} else {
-			fmt.Println("[", entry.ID, "]", entry.Attributes.Status)
+			assert.NotEmpty(t, entry.Anime.Attributes.CanonicalTitle)
 		}
 
 		count++
