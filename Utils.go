6320d9fc1272d8095f8571ba6c09e5ffcc5a484c
Eduard Urbach <e.urbach@gmail.com>
2017-10-24 01:27:04 +0200

    Improved image URLs

--- Utils.go
+++ Utils.go
@@ -5,7 +5,9 @@ import (
 )
 
 // FixImageURL removes the right-most part, e.g. "?1416336000" from image URLs.
+// Additionally it also remove the protocol ("https:")
 func FixImageURL(url string) string {
+	url = strings.TrimPrefix(url, "https:")
 	questionMark := strings.IndexByte(url, '?')
 
 	if questionMark == -1 {
